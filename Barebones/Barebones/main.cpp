1aa58f980b142ddcac00672411731dd0beb4d9ec
Pedrocm99 <pedrocasasdev@gmail.com>
2025-01-11 13:54:26 +0100

    Indexed drawing

--- Barebones/Barebones/main.cpp
+++ Barebones/Barebones/main.cpp
@@ -63,24 +63,35 @@ int main()
 
 	// VERTEX DATA:
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
 	};
 
-	
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
 
 	unsigned int VBO; //Vertex Buffer Object
 	glGenBuffers(1, &VBO);
 
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
 
+	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
 
 	// VERTEX SHADER:
 	unsigned int vertexShader;
@@ -127,10 +138,7 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-
-	
-
-	
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	// RENDER LOOP:
 	while (!glfwWindowShouldClose(window))
@@ -144,7 +152,8 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
